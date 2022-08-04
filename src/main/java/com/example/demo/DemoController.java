aa0aa4a96f8f68218c4cfbcd401c237fd0bf434b
Hector Bedoya Ortegon <neq.hbo@gmail.com>
2022-08-03 23:30:53 -0400

    Ticket-123 implementado SayHello method in DemoController

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam String name) {
+        System.out.println("Saludando a " + name);
+        return "hello " + name;
+    }
+}
